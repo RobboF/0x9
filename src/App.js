b9313b2222c92d9f37f54d344b93303c08d9ae48
Robbo  🦄 <Rob.fisher@outlook.com>
2020-05-23 00:08:54 +0100

    Deployment

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 class='heading'> 0<span style={{fontSize: "30pt", verticalAlign: "10px"}}>x</span>9 </h1>
-        <p> Watch this space</p>
+        <p> Watch this space!</p>
       </header>
     </div>
   );
