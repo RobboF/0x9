8f11601ab8a30be07176fb6f2c4a75df6c6f4600
Robbo  🦄 <Rob.fisher@outlook.com>
2020-05-24 22:41:28 +0100

    Changed logo to SVG and updated slightly

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import logo from './logo.png';
+import logo from './logo.svg'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h1 class='heading'> 0<span style={{fontSize: "45pt", verticalAlign: "12px"}}>x</span>9 </h1>
+        <img src={logo} className="App-logo" alt="logo"/>
+        <h1 className='heading'> 0<span style={{fontSize: "45pt", verticalAlign: "12px"}}>x</span>9 </h1>
         <p> Watch this space</p>
       </header>
     </div>
