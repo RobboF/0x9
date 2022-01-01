1fdab6bc561186d6af6f2eea587537f933b55d95
Rob <rob.fisher@outlook.com>
2022-01-01 23:09:42 +0000

    Updated

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # 0x9 
 
-[![Build Status](https://cloud.drone.io/api/badges/RobboF/0x9/status.svg)](https://cloud.drone.io/RobboF/0x9)
+![Build Status](https://github.com/RobboF/0x9/actions/workflows/on-push/badge.svg)
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import '@testing-library/jest-dom/extend-expect';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+test('renders logo', () => {
+  const { getByAltText } = render(<App />);
+  const linkElement = getByAltText('logo');
   expect(linkElement).toBeInTheDocument();
 });
